abeb5aa94130f5c8bd061290f3d6c134729fe18d
zoubo0502 <zoubo0502@qq.com>
2019-01-29 14:16:46 +0800

    init project

---
+++ src/main/java/com/example/oauth2demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.oauth2demo.controller;
+
+import com.example.oauth2demo.module.TestUser;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(path = "/test/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public TestUser getTestUser(@PathVariable("name") String name) {
+        return new TestUser(name);
+    }
+}
---
+++ src/main/java/com/example/oauth2demo/module/TestUser.java
@@ -0,0 +1,12 @@
+package com.example.oauth2demo.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TestUser {
+    private String name;
+}
